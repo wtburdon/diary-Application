56ffd3cf75e3ae052bee8afdb7eb155cc1b373cd
William Burdon <billyburdon@outlook.com>
2024-10-09 15:31:47 -0700

    Adding functionality to entry display

--- src/Entry.java
+++ src/Entry.java
@@ -2,5 +2,31 @@ import org.json.simple.*;
 
 public class Entry {
     private String fileName;
+    private String title;
+    private String body;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void saveEntry(){
+
+    }
+
+
+    public void loadEntry(){
+
+    }
 }
--- src/entryDisplay.java
+++ src/entryDisplay.java
@@ -1,9 +1,44 @@
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-public class entryDisplay {
+public class entryDisplay extends JFrame {
     private JPanel contentPane;
     private JTextField titleField;
     private JTextArea entryArea1;
     private JScrollBar scrollBar1;
+    private Entry entry;
+
+    public entryDisplay(Entry entry){
+        this.entry = entry;
+        initialize();
+        setTitle("Entry Display");
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        setBounds(100, 100, 450, 300);
+        setVisible(true);
+
+        // Saves Entry before closing the display
+
+        addWindowListener(new WindowAdapter() {
+
+            public void windowClosing(WindowEvent e) {
+                closeEntry();
+                dispose();
+            }
+        });
+    }
+
+    private void initialize(){
+        this.setContentPane(contentPane);
+        titleField.setText(this.entry.getTitle());
+        entryArea1.setText(entry.getBody());
+    }
+
+
+    private void closeEntry(){
+        entry.setTitle(titleField.getText());
+        entry.setBody(entryArea1.getText());
+        entry.saveEntry();
+    }
 
 }
