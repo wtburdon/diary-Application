ce7168e7c4750206c7aed10ae6876badfe350def
William Burdon <billyburdon@outlook.com>
2024-10-08 21:54:50 -0700

    Setup User Class

--- src/User.java
+++ src/User.java
@@ -1,2 +1,9 @@
+import java.util.ArrayList;
+
 public class User {
+    private long id;
+    private int password;
+    private String username;
+    ArrayList<Entry> entries = new ArrayList<>();
+
 }
