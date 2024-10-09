9530c5c1a7a93596974c9a86f5d5099e375a0350
William Burdon <billyburdon@outlook.com>
2024-10-09 15:32:19 -0700

    Creating login sequence

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        //Entry testEntry = new Entry();
+
+        //entryDisplay test = new entryDisplay(testEntry);
+
+        defaultWindow w = new defaultWindow();
 
     }
 
--- src/defaultWindow.java
+++ src/defaultWindow.java
@@ -1,12 +1,42 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class defaultWindow{
-    public int width,height;
+public class defaultWindow extends JFrame {
+    private JPanel contentPane;
+    private JButton loginButton;
+    private JPasswordField passwordField1;
+    private JTextField usernameField1;
+    private JLabel passwordLabel;
+    private JLabel usernameLabel;
 
     public defaultWindow() {
 
+        initialize();
+
+        this.setTitle("Login Window");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setBounds(100, 100, 450, 300);
+        this.setVisible(true);
+
+        //Allows for login
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                loginAttempt();
+            }
+        });
+    }
+
+    private void initialize(){
+        this.setContentPane(contentPane);
     }
 
 
+    private void loginAttempt(){
+        if(usernameField1.getText().equals("admin") && passwordField1.getText().equals("admin")){
+            System.out.println("Login Successful");
+        }
+    }
 
 }
