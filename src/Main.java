6e58d93087b274a0f40904909efbaef24e17f3f4
William Burdon <billyburdon@outlook.com>
2024-10-09 21:06:54 -0700

    commented out tests for functionality

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 public class Main {
+
+    public static void accessUsers(){
+
+    }
+
+
     public static void main(String[] args) {
         //Entry testEntry = new Entry();
 
         //entryDisplay test = new entryDisplay(testEntry);
 
-        defaultWindow w = new defaultWindow();
+        //defaultWindow w = new defaultWindow();
 
+        //createAccountWindow c = new createAccountWindow();
     }
-
-
-
 }
