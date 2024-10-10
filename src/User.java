380ff455bf6c96d896b54e79f55f931b42479253
William Burdon <billyburdon@outlook.com>
2024-10-09 19:01:42 -0700

    implementing username and password functionality

--- src/User.java
+++ src/User.java
@@ -1,9 +1,25 @@
 import java.util.ArrayList;
 
 public class User {
-    private long id;
-    private int password;
+    private String password;
     private String username;
     private ArrayList<Entry> entries = new ArrayList<>();
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean checkPassword(String password) {
+        return this.password.equals(password);
+    }
+
+    public boolean checkUsername(String username) {
+        return this.username.equals(username);
+    }
 
 }
