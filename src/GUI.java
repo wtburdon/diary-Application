84892bf0f35a04230908b46225ed19d436403f52
William Burdon <billyburdon@outlook.com>
2024-10-09 21:48:53 -0700

    added static methods to be implemented later

--- src/GUI.java
+++ src/GUI.java
@@ -2,11 +2,20 @@ import javax.swing.*;
 
 public class GUI {
 
+    public static void start(){
 
 
+    }
 
-    public void start(){
+    public static void displayEntryList(){
 
+    }
+
+    public static void displayDefaultWindow(){
+
+    }
+
+    public static void displayEntry(){
 
     }
 }
