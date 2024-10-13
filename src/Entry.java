2207ea9b965b755a7361f95f6e17ababcc08a3d7
William Burdon <billyburdon@outlook.com>
2024-10-12 19:58:43 -0700

    implemented entry save and load functions

--- src/Entry.java
+++ src/Entry.java
@@ -1,5 +1,12 @@
 import org.json.simple.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
 public class Entry {
     private String fileName;
     private String title;
@@ -22,11 +29,34 @@ public class Entry {
     }
 
     public void saveEntry(){
+        Path p = Paths.get("data").resolve(this.title);
+        File file = p.toFile();
+
+        file.delete();
+        try {
+            file.createNewFile();
+            Files.write(file.toPath(), body.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
+    
+    // FILE NAME SHOULD BE SET TO TITLE NAME
+    public static Entry loadEntry(String filename){
+        Path p = Paths.get("data").resolve(filename);
+        String entryData;
+        try {
+            entryData = Files.readString(p);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Entry entry = new Entry();
+        entry.setBody(entryData);
+        entry.setTitle(filename);
 
 
-    public void loadEntry(){
-
+        return entry;
     }
 }
